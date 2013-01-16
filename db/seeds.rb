ec6ba40bd6e66a74fa1cc1bfb391461073cad008
Diego Nalini <diegonalini@geocities.com>
2013-01-15 16:14:54 -0800

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Environment variables (ENV['...']) are set in the file config/application.yml.
+# See http://railsapps.github.com/rails-environment-variables.html
+puts 'DEFAULT USERS'
+user = User.create! name:"admin", email:"oid@gmail.com", password:"", password_confirmation:""
+puts 'user: ' << user.name
